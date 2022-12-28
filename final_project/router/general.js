5b1d71ef4688620ac05cf1decf221fa0c1f72223
Lian Aldrich <aldrich@neoway.tech>
2022-12-28 13:22:54 +0200

    Post elementary data retrieval, prior to authentication and authorization implementation.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,71 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
+
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  if (books) {
+    res.send(books);
+  }
+  else {
+    return res.status(404).json({message: "Books Database is Empty"});
+  }
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  book = books[isbn];
+  if (book) {
+    res.send(books[isbn]);
+  }
+  else {
+    return res.status(404).json({message: "ISBN Not Found"});
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  author = req.params.author;
+  arrBooks = Object.values(books);
+  //let booksByAuthor = arrBooks.filter(book => new RegExp(author, 'i').test(book.author)); //This is a potentially unsafe way to do this. Would be better to convert both strings to lowercase and then compare - see below
+  let booksByAuthor = arrBooks.filter(book => book.author.toLowerCase().includes(author.toLowerCase()));
+
+  if (booksByAuthor.length > 0) {
+    res.send(booksByAuthor);
+  }
+  else {
+    return res.status(404).json({message: "No books by this author found"});
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  title = req.params.title;
+  arrBooks = Object.values(books);
+  let booksByTitle = arrBooks.filter(book => book.title.toLowerCase().includes(title.toLowerCase()));
+
+  if (booksByTitle.length > 0) {
+    res.send(booksByTitle);
+  }
+  else {
+    return res.status(404).json({message: "No books with this title found"});
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  book = books[isbn];
+  if (Object.keys(book["reviews"]).length > 0) {
+    res.send(book["reviews"]);
+  }
+  else {
+    return res.status(404).json({message: "This book has no reviews"});
+  }
 });
 
 module.exports.general = public_users;
