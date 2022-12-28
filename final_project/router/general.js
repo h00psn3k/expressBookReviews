3bd45dd7e98979acc48455692846aa4e13203341
Lian Aldrich <aldrich@neoway.tech>
2022-12-28 21:51:28 +0200

    After Task 9

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,25 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+public_users.use(express.json({extended: true}));
+public_users.use(express.urlencoded({ extended: true }));
 
+const arrBooks = Object.values(books);
+
+// Register a new user
 public_users.post("/register", (req,res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-  return res.status(300).json({message: "Yet to be implemented"});
+  if (username && password) {
+    if (!isValid(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});    
+    }
+  } 
+  return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -22,7 +37,7 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
+  let isbn = req.params.isbn;
   book = books[isbn];
   if (book) {
     res.send(books[isbn]);
@@ -34,8 +49,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  author = req.params.author;
-  arrBooks = Object.values(books);
+  let author = req.params.author;
   //let booksByAuthor = arrBooks.filter(book => new RegExp(author, 'i').test(book.author)); //This is a potentially unsafe way to do this. Would be better to convert both strings to lowercase and then compare - see below
   let booksByAuthor = arrBooks.filter(book => book.author.toLowerCase().includes(author.toLowerCase()));
 
@@ -49,8 +63,7 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  title = req.params.title;
-  arrBooks = Object.values(books);
+  let title = req.params.title;
   let booksByTitle = arrBooks.filter(book => book.title.toLowerCase().includes(title.toLowerCase()));
 
   if (booksByTitle.length > 0) {
